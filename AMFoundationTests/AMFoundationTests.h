2de863a8ef5a8a804eaa61ad0ad1e7c77f3dea6b
Andy Mroczkowski <andy@mrox.net>
2011-04-23 15:20:19 -0700

    Initial Commit

---
+++ AMFoundationTests/AMFoundationTests.h
@@ -0,0 +1,17 @@
+//
+//  AMFoundationTests.h
+//  AMFoundationTests
+//
+//  Created by Andy Mroczkowski on 4/23/11.
+//  Copyright 2011 MindSnacks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface AMFoundationTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ AMFoundationTests/AMFoundationTests.m
@@ -0,0 +1,33 @@
+//
+//  AMFoundationTests.m
+//  AMFoundationTests
+//
+//  Created by Andy Mroczkowski on 4/23/11.
+//  Copyright 2011 MindSnacks. All rights reserved.
+//
+
+#import "AMFoundationTests.h"
+
+
+@implementation AMFoundationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AMFoundationTests");
+}
+
+@end
