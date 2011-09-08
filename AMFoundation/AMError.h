c3530e56c23625e4a8f300900ef2be6bebeb6cbc
Andy Mroczkowski <andy@mrox.net>
2011-09-08 08:12:33 -0700

    adding domain to AMError

--- AMFoundation/AMError.h
+++ AMFoundation/AMError.h
@@ -14,15 +14,8 @@ enum
 	AMERR_WRAPPED = 1,	
 };
 
-#define AMError(S) _AMErrorMake(S, #S, __FILE__, __LINE__, nil)
-#define AMErrorWithInfo(S,T) _AMErrorMake(S, #S, __FILE__, __LINE__, T)
-#define AMErrorP(S,T) AMErrorAssignIfNotNil(S, AMError(T))
-#define AMErrorWithInfoP(S,T,U) AMErrorAssignIfNotNil(S, AMErrorWithInfo(T,U))
-    
-
-#define _AMErrorWrapError(S) AMErrorWithInfo(AMERR_WRAPPED, [NSDictionary dictionaryWithObject:S forKey:NSUnderlyingErrorKey])
-#define AMErrorWrapError(S) (S = _AMErrorWrapError(S))
-#define AMErrorWrapErrorP(S) AMErrorAssignIfNotNil(S, (S != NULL) ? _AMErrorWrapError(*S) : NULL)
+#define AMError(E,D) _AMErrorMake(E, #E, D, __FILE__, __LINE__, nil)
+#define AMErrorWithInfo(E,D,I) _AMErrorMake(E, #E, D, __FILE__, __LINE__, I)
 
 #define AMErrorAddOriginToError(S) (S = _AMErrorAddOrigin(S, __FILE__, __LINE__))
 #define AMErrorAddOriginToErrorP(S) AMErrorAssignIfNotNil(S, (S != NULL) ? AMErrorAddOriginToError(*S) : NULL)
@@ -37,7 +30,7 @@ static inline void AMErrorAssignIfNotNil( NSError** outError, NSError* error )
 	
 
 // internal methods, use macros
-extern NSError* _AMErrorMake( int errorCode, char const *errorName,  char const *fileName, int lineNumber, NSDictionary *userInfo );	
+extern NSError* _AMErrorMake( int errorCode, char const *errorName, NSString* domain, char const *fileName, int lineNumber, NSDictionary *userInfo );	
 extern NSError* _AMErrorAddOrigin( NSError* origError, char const *fileName, int lineNumber );
 
 	
