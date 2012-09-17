ec9efb4f7c351e8ee8fb3b328559e10bcbe44759
Andy Mroczkowski <andy@mrox.net>
2012-09-16 23:31:38 -0700

    Renaming log macros

--- AMFoundation/AMLogMacros.h
+++ AMFoundation/AMLogMacros.h
@@ -9,14 +9,14 @@
 
 
 #ifdef DEBUG
-	#define LOG_DEBUG(fmt, ...)    (NSLog(fmt, ##__VA_ARGS__));
-	#define LOG_TRACE()            (NSLog(@"%s:%d", __FILE__, __LINE__));
+	#define AMLOG_DEBUG(fmt, ...)    (NSLog(fmt, ##__VA_ARGS__));
+	#define AMLOG_TRACE()            (NSLog(@"%s:%d", __FILE__, __LINE__));
 #else
-	#define LOG_DEBUG(...)
-	#define LOG_TRACE()
+	#define AMLOG_DEBUG(...)
+	#define AMLOG_TRACE()
 #endif
 
-#define LOG_WARNING(fmt, ...)   (NSLog(@"WARN: %s " fmt, __PRETTY_FUNCTION__, ##__VA_ARGS__));
-#define LOG_ERROR(fmt, ...)		(NSLog(@"ERR: %s " fmt, __PRETTY_FUNCTION__, ##__VA_ARGS__));
+#define AMLOG_WARNING(fmt, ...)   (NSLog(@"WARN: %s " fmt, __PRETTY_FUNCTION__, ##__VA_ARGS__));
+#define AMLOG_ERROR(fmt, ...)		(NSLog(@"ERR: %s " fmt, __PRETTY_FUNCTION__, ##__VA_ARGS__));
 
 #define AMFourCC2Str(code) (char[5]){(code >> 24) & 0xFF, (code >> 16) & 0xFF, (code >> 8) & 0xFF, code & 0xFF, 0}
